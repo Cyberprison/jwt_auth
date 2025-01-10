e5cea9a23311daa3c27dcbe02227e24c034f656f
Хтоты Хтоя <az.khabibullin2018@gmail.com>
2025-01-10 22:55:09 +0500

    доделал по видосу сервис пользователя
    вызов в контроллере
    
    надо в постмане протестировать, но лень, позже доделаю
    
    сваггер надо изучить, и как-то добавить в интерфейс сваггера поля для заголовков запроса

--- server/service/token-service.js
+++ server/service/token-service.js
@@ -1,5 +1,41 @@
-class TokenService {
+const jwt = require("jsonwebtoken");
+const tokenModel = require("../models/token-model");
 
+class TokenService {
+    generateTokens(payload) {
+        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
+            expiresIn: "30m"
+        });
+        const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
+            expiresIn: "30d"
+        });
+        return {
+            accessToken,
+            refreshToken
+        }
+    }
+
+    //если токен есть то перезапись, 
+    //а с того другого устройства, на котором был вход,
+    //будет вылет
+    //если нужно другое поведение, то надо продумать БД
+    //для сохранения нескольких токенов, удаления старых
+    async saveToken(userId, refreshToken) {
+        const tokenData = await tokenModel.findOne({
+            user: userId
+        });
+        if (tikenData) {
+            tokenData.refreshToken = refreshToken;
+            return tokenData.save();
+        }
+
+        const token = await tokenModel.create({
+            user: userId,
+            refreshToken
+        });
+
+        return token;
+    }
 
 }
 
